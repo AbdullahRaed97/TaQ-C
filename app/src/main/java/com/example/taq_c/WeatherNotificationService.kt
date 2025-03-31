f1b0938909d70f2b691e1dad34f23a40a1f7a74a
Abdullah Raed <abdullahraediti@gmail.com>
2025-03-31 07:27:05 +0200

    implement WeatherNotificationService

---
+++ app/src/main/java/com/example/taq_c/WeatherNotificationService.kt
@@ -0,0 +1,9 @@
+package com.example.taq_c
+
+class WeatherNotificationService {
+
+    companion object{
+        const val WEATHER_CHANNEL_ID = "Weather_Channel"
+        const val NOTIFICATION_ID = 1
+    }
+}
